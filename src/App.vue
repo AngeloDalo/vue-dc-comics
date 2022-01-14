048704d7408960b37c20d9d2954368ae691a89c3
AngeloDalo <angelo251158@gmail.com>
2022-01-14 14:39:32 +0100

    titles

--- src/App.vue
+++ src/App.vue
@@ -38,5 +38,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 5px;
+  min-width: 1269px;
 }
 </style>
---
+++ src/components/MainTitles.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="title">
+        <img class="title-image"
+        :src="src"
+        :alt="alt">
+        <h2> {{ title }} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MainTitles",
+    props: ['src', 'alt', 'title'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
